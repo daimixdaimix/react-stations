8546dcee05e076de02b93d1ac7e261a188fb6e34
DaisukeMatsuda <daimix@hotmail.com>
2025-10-09 13:59:10 +0900

    App jsx 犬のアプリの入力

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,11 @@ export const App = () => {
   return (
     <>
       <header>
-        <h1>猫のアプリ</h1>
+        <h1>犬のアプリ</h1>
       </header>
+      <div>
+        <p>ここに犬の画像を表示します。</p>
+        <img src='https://images.dog.ceo/breeds/collie-border/n02106166_1539.jpg' alt='犬の画像' />      </div>
     </>
   )
 }
