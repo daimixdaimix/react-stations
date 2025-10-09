81222b99f002e3e4971ed3ae2ba6041ce39a016f
DaisukeMatsuda <daimix@hotmail.com>
2025-10-09 18:25:38 +0900

    cssの追加

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,22 @@ import { useState } from 'react';
 const App = () => {
   const [dogUrl, setDogUrl] = useState('https://images.dog.ceo/breeds/collie-border/n02106166_1539.jpg');
 
+  const fetchRandomDogImage = () => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setDogUrl(data.message))
+      .catch(error => console.error('Error fetching the dog image:', error));
+  };
+
   return (
     <>
-      <header>
+      <header className="header-style">
         <h1>犬のアプリ</h1>
       </header>
       <div>
-        <p>ここに犬の画像を表示します。</p>
+        <p className="dog-text">ここに犬の画像を表示します。</p>
         <img src={dogUrl} alt='犬の画像' />
-        <button onClick={() => setDogUrl('https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg')}>犬の画像を変更</button>
+        <button onClick={fetchRandomDogImage}>犬の画像を変更</button>
       </div>
     </>
   )
