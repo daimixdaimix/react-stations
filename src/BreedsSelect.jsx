d7a72dc3fc620fc9868342adb61675ea71aa31cc
DaisukeMatsuda <daimix@hotmail.com>
2025-10-14 16:49:34 +0900

    ドロップコンポーネントの作成その2

--- src/BreedsSelect.jsx
+++ src/BreedsSelect.jsx
@@ -1,7 +1,18 @@
 // @ts-check
 
-export const BreedsSelect = () => {
-  return <></>
+export const BreedsSelect = (props) => {
+  const { breeds } = props;
+  return (
+    <select>
+      <option value="">Select a breed</option>
+      
+      {
+      /* ここにoptionタグをmapで生成 */
+      breeds.map((breed) => (
+        <option key={breed} value={breed}>{breed}</option>
+      ))}
+    </select>
+  )
 }
 
 export default BreedsSelect
